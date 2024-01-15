e2bd0b7682113c6f82257607fbb3558b617f23b5
meirVitkin <vitkin4580@gmail.com>
2024-01-15 19:04:00 +0200

    some progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import { useState } from 'react';
 import './App.css'
 import Layout from './Layout'
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
 
 function App() {
-  const [user, setUser] = useState({});
 
   return (
-    <>
-    {Object.keys(user).length === 0 && (
-      <GoogleLogin
-        onSuccess={credentialResponse => {
-         const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
-         setUser(credentialResponseDecoded);
-         console.log(credentialResponseDecoded);
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-      />
-
-    )}
-      {Object.keys(user).length > 0 &&(
-
-      <Layout />
-      )}
-    </>
+    <Layout />
+    
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,47 @@
+import { useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+
 const Header = () => {
-   
+    const [user, setUser] = useState({});
+    const [login, setLogin] = useState(false);
+    const [loginClick, setLoginClick] = useState(false);
+    const handelLoginClick = () => {
+        setLoginClick(!loginClick)
+        Object.keys(user).length > 0 &&(
+            setLogin(!login)
+            )
+    }
+    const handelLogOutClick = () => {
+        setLoginClick(!loginClick)
+        setLogin(!login);
+    }
+
     return (
         <div className='header'>
+            {!login ? <div onClick={handelLoginClick}>Login</div> : <div onClick={handelLogOutClick}>Log out</div>}
+            {user.name && <div>{user.name}</div>}
             <h1>☘ GROCERY STORE ☘</h1>
+            <>
+            {loginClick &&(
+                 <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+                            setUser(credentialResponseDecoded);
+                            console.log(credentialResponseDecoded);
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                    />
+            )}
+                {/* {Object.keys(user).length === 0 && (
+                   
+
+                )} */}
+                {/* {Object.keys(user).length > 0 &&(
+      )} */}
+            </>
         </div>
     )
 }
